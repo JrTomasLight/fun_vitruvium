ab3dbd57054ccf9741ee360591fffe09c0f8bd36
JrTomasLight <aleksandrignatev167@gmail.com>
2026-03-11 22:50:08 +0300

    Update item-sheet.mjs

--- fun_vitruvium/module/sheets/item-sheet.mjs
+++ fun_vitruvium/module/sheets/item-sheet.mjs
@@ -1,93 +1,37 @@
-import {
-  onManageActiveEffect,
-  prepareActiveEffectCategories,
-} from '../helpers/effects.mjs';
-
-/**
- * Extend the basic ItemSheet with some very simple modifications
- * @extends {ItemSheet}
- */
-export class BoilerplateItemSheet extends ItemSheet {
+export class VitruviumItemSheet extends ItemSheet {
   /** @override */
   static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ['boilerplate', 'sheet', 'item'],
+    return mergeObject(super.defaultOptions, {
+      classes: ['vitruvium', 'sheet', 'item'],
       width: 520,
       height: 480,
-      tabs: [
-        {
-          navSelector: '.sheet-tabs',
-          contentSelector: '.sheet-body',
-          initial: 'description',
-        },
-      ],
+      tabs: [{ navSelector: '.sheet-tabs', contentSelector: '.sheet-body', initial: 'description' }]
     });
   }
 
   /** @override */
   get template() {
-    const path = 'systems/boilerplate/templates/item';
-    // Return a single sheet for all item types.
-    // return `${path}/item-sheet.hbs`;
-
-    // Alternatively, you could use the following return statement to do a
-    // unique item sheet by type, like `weapon-sheet.hbs`.
-    return `${path}/item-${this.item.type}-sheet.hbs`;
+    return `systems/vitruvium/templates/item-${this.item.type}.hbs`;
   }
-
-  /* -------------------------------------------- */
 
   /** @override */
   async getData() {
-    // Retrieve base data structure.
     const context = super.getData();
-
-    // Use a safe clone of the item data for further operations.
-    const itemData = this.document.toObject(false);
-
-    // Enrich description info for display
-    // Enrichment turns text like `[[/r 1d20]]` into buttons
-    context.enrichedDescription = await TextEditor.enrichHTML(
-      this.item.system.description,
-      {
-        // Whether to show secret blocks in the finished html
-        secrets: this.document.isOwner,
-        // Necessary in v11, can be removed in v12
-        async: true,
-        // Data to fill in for inline rolls
-        rollData: this.item.getRollData(),
-        // Relative UUID resolution
-        relativeTo: this.item,
-      }
-    );
-
-    // Add the item's data to context.data for easier access, as well as flags.
+    const itemData = context.item;
+    
     context.system = itemData.system;
-    context.flags = itemData.flags;
-
-    // Adding a pointer to CONFIG.BOILERPLATE
-    context.config = CONFIG.BOILERPLATE;
-
-    // Prepare active effects for easier access
-    context.effects = prepareActiveEffectCategories(this.item.effects);
-
+    context.config = CONFIG.vitruvium;
+    
     return context;
   }
-
-  /* -------------------------------------------- */
 
   /** @override */
   activateListeners(html) {
     super.activateListeners(html);
-
-    // Everything below here is only needed if the sheet is editable
-    if (!this.isEditable) return;
-
-    // Roll handlers, click handlers, etc. would go here.
-
-    // Active Effect management
-    html.on('click', '.effect-control', (ev) =>
-      onManageActiveEffect(ev, this.item)
-    );
+    
+    // Здесь можно добавить обработчики для специфичных элементов
+    if ( this.isEditable ) {
+      // Например, кнопки для типов способностей
+    }
   }
 }
